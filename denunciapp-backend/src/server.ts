5b019da0142613790cec7888133c4853e92141f8
LeonardoPerozo12 <leonardo.perozzof@gmail.com>
2025-04-09 14:40:07 -0400

    refactor: simplify CORS configuration and improve request logging in server setup

--- denunciapp-backend/src/server.ts
+++ denunciapp-backend/src/server.ts
@@ -8,32 +8,29 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT 
 
-    connectDB(); // Connect to the database
-    console.log('Database connected successfully');
-    // CROSS ORIGIN RESOURCE SHARING configuration
-    app.use(cors({ 
-        origin: process.env.FRONTEND_URL, // Permitir peticiones desde el frontend
-        credentials: true,
+connectDB(); // Connect to the database
+console.log('Database connected successfully');
+// CROSS ORIGIN RESOURCE SHARING configuration
+app.use(cors({
+    origin: '*', // Permitir todos los orígenes
+    credentials: true,
     }));
-
-    app.use(express.json()); // Middleware to parse JSON requests
-
-    //Monta las rutas
-    routes.forEach((route, index) => {
-        app.use(`/api`, route); // Monta cada ruta en el prefijo /api
-    });
     
-    app.get("/", (req, res) => {
-        res.send("<h1>API de DenunciApp<h1>");
-    });
-
-
-    app.listen(port, () => {
-        console.log(`Now listening on port http://localhost:${port}` );
-    });
-
-    // Log incoming requests
+    app.use(express.json());
+    
     app.use((req, res, next) => {
-        console.log(`[${req.method}] ${req.url} - ${new Date().toISOString()}`);
-        next();
+    console.log(`[${req.method}] ${req.url} - ${new Date().toISOString()}`);
+    next();
     });
+
+routes.forEach((route, index) => {
+    app.use(`/api`, route);
+});
+
+app.get("/", (req, res) => {
+    res.send("<h1>API de DenunciApp<h1>");
+});
+
+app.listen(port, () => {
+    console.log(`Now listening on port http://localhost:${port}`);
+});
