9c3d6754c9b965aab20e1ad8974cc9fe4401e1cc
LeonardoPerozo12 <leonardo.perozzof@gmail.com>
2025-04-23 11:57:04 -0400

    Revert "refactor: simplify CORS configuration and improve request logging in server setup"
    This reverts commit 5b019da0142613790cec7888133c4853e92141f8.

--- denunciapp-backend/src/server.ts
+++ denunciapp-backend/src/server.ts
@@ -15,22 +15,25 @@ app.use(cors({
     origin: '*',// Permitir todos los orígenes
     credentials: true,
     }));
+
+    app.use(express.json()); // Middleware to parse JSON requests
+
+    //Monta las rutas
+    routes.forEach((route, index) => {
+        app.use(`/api`, route); // Monta cada ruta en el prefijo /api
+    });
     
-    app.use(express.json());
-    
-    app.use((req, res, next) => {
-    console.log(`[${req.method}] ${req.url} - ${new Date().toISOString()}`);
-    next();
+    app.get("/", (req, res) => {
+        res.send("<h1>API de DenunciApp<h1>");
     });
 
-routes.forEach((route, index) => {
-    app.use(`/api`, route);
-});
 
-app.get("/", (req, res) => {
-    res.send("<h1>API de DenunciApp<h1>");
-});
+    app.listen(port, () => {
+        console.log(`Now listening on port http://localhost:${port}` );
+    });
 
-app.listen(port, () => {
-    console.log(`Now listening on port http://localhost:${port}`);
-});
+    // Log incoming requests
+    app.use((req, res, next) => {
+        console.log(`[${req.method}] ${req.url} - ${new Date().toISOString()}`);
+        next();
+    });
